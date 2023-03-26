def75242c1b760a23c80ac619e76f4bbdd960cb9
Brett Kuhn <brettkuhn87@gmail.com>
2023-03-26 01:02:03 -0600

    Adds initial navigation bar styling

--- BAK-Portfolio/src/Components/NavBar/NavBar.tsx
+++ BAK-Portfolio/src/Components/NavBar/NavBar.tsx
@@ -3,7 +3,28 @@ import '../NavBar/NavBar.css'
 
 const NavBar = () => {
   return (
-    <h1>NAVBAR</h1>
+    <nav className='flex-col flex justify-end w-full border-8 border-black'>
+      <h1 className='border-4 border-green-600 text-right text-6xl'>
+        Hi, I'm Brett Kuhn.
+      </h1>
+      <section className='w-full flex-row flex justify-around'>
+        <button className='cursor-pointer border-8 border-indigo-500'>
+          About Me
+        </button>
+        <button>
+          Contact
+        </button>
+        <button>
+          Resume
+        </button>
+        <button>
+          Projects
+        </button>
+        <button>
+          Skills
+        </button>
+      </section>
+    </nav>
   )
 }
 
