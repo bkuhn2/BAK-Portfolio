ab77b863ad79f67c3a172f3ef93a923cd009496c
Brett Kuhn <brettkuhn87@gmail.com>
2023-03-26 13:59:33 -0600

    Adds about me preliminary content

--- BAK-Portfolio/src/Components/AboutSection/AboutSection.tsx
+++ BAK-Portfolio/src/Components/AboutSection/AboutSection.tsx
@@ -3,7 +3,32 @@ import '../AboutSection/AboutSection.css'
 
 const AboutSection = () => {
   return (
-    <h1 id='about-section'>About SSSSection</h1>
+    <section id='about-section'>
+      <div>
+        <img />
+      </div>
+      <div>
+        <h1>About</h1>
+        <br />
+        <h2>Who I Am</h2>
+        <p>Hello! I'm Brett Kuhn, a software engineer who specializes in front end development and a recent graduate of 
+          Turing School of Software and Design. I'm passionate about building applications and solving complex 
+          problems, and I find great joy in new experiences and learning new things, both inside and outside of the 
+          tech industry. Working as a part of a team is important to me, and I take pride in building a strong
+          culture and fostering collaboration. When I'm not coding, you can find me traveling, rock climbing, playing
+          music or building guitars.
+          <br />
+          Talk about career transition here...? 
+          After nearly a decade in the oil and gast industry..
+        </p>
+        <br />
+        <h2>What I'm Working On</h2>
+        <ul>
+          <li>Building...</li>
+          <li>Learning...</li>
+        </ul>
+      </div>
+    </section>
   )
 }
 
--- BAK-Portfolio/src/Components/NavBar/NavBar.tsx
+++ BAK-Portfolio/src/Components/NavBar/NavBar.tsx
@@ -3,13 +3,22 @@ import { Link } from 'react-router-dom'
 import '../NavBar/NavBar.css'
 
 const NavBar = () => {
+
+  //useNavigate? or NavLink, be active?
+  //Navigate component?
+  //Outlet Component?
+  //use location?
+  //generate path?
+
   return (
     <nav className='flex-col grid justify-items-end w-full border-8 border-black'>
       <h1 className='border-4 border-green-600 text-right text-6xl w-fit '>
         Hi, I'm Brett Kuhn.
       </h1>
       <section className='w-full flex-row flex justify-around'>
-        <button className='cursor-pointer border-8 border-indigo-500 w-52'>
+        <button className='cursor-pointer border-8 border-indigo-500 w-52'
+          // onClick={}
+        >
           About Me
         </button>
         <button>
