e29d9a260d2bee363d150e8066cb597df146aed3
Brett Kuhn <brettkuhn87@gmail.com>
2023-03-27 14:45:32 -0600

    Adds background image to navbar

--- BAK-Portfolio/src/Components/NavBar/NavBar.tsx
+++ BAK-Portfolio/src/Components/NavBar/NavBar.tsx
@@ -4,13 +4,14 @@ import '../NavBar/NavBar.css'
 
 const NavBar = () => {
 
+
   return (
-    <nav className='bg-blackTexture bg-fixed fixed flex-col grid justify-items-end w-full border-8 border-black'>
+    <nav className='h-1/4 bg-black-texture3 bg-no-repeat bg-cover bg-center bg-fixed fixed flex-col grid justify-items-end w-full border-8 border-black'>
       <h1 className='border-4 border-green-600 text-right text-6xl w-fit '>
         Hi, I'm Brett Kuhn.
       </h1>
       <section className='w-full flex-row flex justify-around'>
-        <NavLink to='/'>About</NavLink>
+        <NavLink className='text-zinc-50 border-4 border-green-600' to='/'>About</NavLink>
         <NavLink to='/contact'>Contact</NavLink>
         <NavLink to='/projects'>Projects</NavLink>
         <NavLink to='/skills'>SKills</NavLink>
--- BAK-Portfolio/tailwind.config.cjs
+++ BAK-Portfolio/tailwind.config.cjs
@@ -7,7 +7,9 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'blackTexture': 'url(src/assets/malik-skydsgaard-jY9rX-E7ztU-unsplash (1).jpg)'
+        'black-texture1': "url('../src/assets/annie-spratt-6a3nqQ1YwBw-unsplash.jpg')",
+        'black-texture2': "url('../src/assets/andrej-lisakov-g3z-CgUiPtg-unsplash (1).jpeg')",
+        'black-texture3': "url('../src/assets/3AD0D639-0F74-4C13-8824-D5AD4B150A9A.jpeg')"
       }
     },
   },
