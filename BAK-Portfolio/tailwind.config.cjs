7d692330f5076740b0ac3bf3e7589cac76bc1247
Brett Kuhn <brettkuhn87@gmail.com>
2023-03-27 23:59:16 -0600

    Removes unused images

--- BAK-Portfolio/tailwind.config.cjs
+++ BAK-Portfolio/tailwind.config.cjs
@@ -8,9 +8,6 @@ module.exports = {
     extend: {
       backgroundImage: {
         'black-texture1': "url('../src/assets/annie-spratt-6a3nqQ1YwBw-unsplash.jpg')",
-        'black-texture2': "url('../src/assets/andrej-lisakov-g3z-CgUiPtg-unsplash (1).jpeg')",
-        'black-texture3': "url('../src/assets/3AD0D639-0F74-4C13-8824-D5AD4B150A9A.jpeg')",
-        'desktop-1': "url('../src/assets/desktopA.jpg')",
         'desktop-2': "url('../src/assets/desktopB.jpg')",
         'desktop-3': "url('../src/assets/desktopC.jpg')",
         'main6': "url('../src/assets/main6.jpg')",
