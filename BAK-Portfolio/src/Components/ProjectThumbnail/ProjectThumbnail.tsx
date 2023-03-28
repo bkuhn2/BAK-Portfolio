65957ad3db9c54855c7010a2c58c887033a205ee
Brett Kuhn <brettkuhn87@gmail.com>
2023-03-28 17:40:58 -0600

    Styles project thumbnails, and creates thumbnail renders on projects page

--- BAK-Portfolio/src/Components/ProjectThumbnail/ProjectThumbnail.tsx
+++ BAK-Portfolio/src/Components/ProjectThumbnail/ProjectThumbnail.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import '../ProjectThumbnail/ProjectThumbnail.css'
+import { Project } from '../../Helper/Interfaces'
 
-const ProjectThumbnail = () => {
+type Prop = {
+  project: Project
+}
+
+const ProjectThumbnail = ({project}: Prop) => {
+
   return (
-    <div>
-      
-    </div>
+    <div className='hover:cursor-pointer flex flex-col justify-between items-center p-2 h-full w-5/6 border-2 border-black rounded-lg bg-gradient-to-t from-neutral-900 to-stone-700 drop-shadow-lg'>
+      <img 
+        className='mx-4 md:mx-7 lg:mx-10 border-4 border-stone-800 rounded-lg shadow-inner contrast-75 brightness-75 hover:contrast-100 hover:brightness-110'
+        src={project.img} 
+        alt=''/>
+      <p className='text-5xl my-5 text-amber-500 text-center underline decoration-red-600 decoration-4'>{project.name}</p>
+  </div>
   )
 }
 
--- BAK-Portfolio/src/Components/ProjectsPage/ProjectsPage.tsx
+++ BAK-Portfolio/src/Components/ProjectsPage/ProjectsPage.tsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from '../NavBar/NavBar'
 import '../ProjectsPage/ProjectsPage.css'
+import { brettProjects } from '../../data/brettProjectsData'
+import { Project } from '../../Helper/Interfaces'
+import ProjectThumbnail from '../ProjectThumbnail/ProjectThumbnail'
 
 const ProjectsPage = () => {
+
+  const [allProjects, setProjects] = useState<Project[]>();
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState('');
+    
+  useEffect(()=> {
+    setProjects(brettProjects)
+  }, []);
+
+  const thumbnails = allProjects ? allProjects.map((project, index) => {
+    return (
+      <ProjectThumbnail project={project} key={index}/>
+    )
+  }) : '';
+
+
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <section className='flex flex-col items-center'>
+        <br />
+        <h1 className='border-2 border-stone-300 bg-white text-2xl md:text-5xl bg-opacity-60 backdrop-blur-sm w-screen py-3 text-stone-800 font-bold text-center mb-10 drop-shadow-sm'>
+          Projects</h1>
+        <br />
+        <form className='border-2 border-red-500'>
+          <p>Type in a language or technology to find any projects that match.</p>
+          <input />
+        </form>
+        <br /><br />
+        <section className='w-full grid grid-cols-2 gap-y-10 md:gap-y-20 justify-items-center justify-evenly p-1 mb-5'>
+          {/*Make conditional .. loading, no results, etc */}
+          {(!searchResults && !thumbnails) && <h2>wtf mate</h2>}
+          {(!searchResults && thumbnails) && thumbnails}
+        </section>
+      </section>
+    </>
   )
 }
 
