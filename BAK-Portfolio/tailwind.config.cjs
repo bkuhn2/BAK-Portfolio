6ef13789e9f95f9daced9e051f83a23113f32094
Brett Kuhn <brettkuhn87@gmail.com>
2023-03-28 17:39:04 -0600

    Added custom style extions

--- BAK-Portfolio/tailwind.config.cjs
+++ BAK-Portfolio/tailwind.config.cjs
@@ -7,12 +7,13 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'black-texture1': "url('../src/assets/annie-spratt-6a3nqQ1YwBw-unsplash.jpg')",
-        'desktop-2': "url('../src/assets/desktopB.jpg')",
-        'desktop-3': "url('../src/assets/desktopC.jpg')",
-        'main6': "url('../src/assets/main6.jpg')",
         'main7': "url('../src/assets/main7.jpg')"
-      }
+      },
+      spacing: {
+        '110': '28rem',
+        '120': '32rem'
+
+      },
     },
   },
   plugins: [],
