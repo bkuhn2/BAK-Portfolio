7fdc93e3c4a9181e2d415ae3ea1c3058a6341593
Brett Kuhn <brettkuhn87@gmail.com>
2023-03-28 11:38:57 -0600

    Adds styling tin LinkedIn and GH ilnks

--- BAK-Portfolio/src/Components/ContactSection/ContactSection.tsx
+++ BAK-Portfolio/src/Components/ContactSection/ContactSection.tsx
@@ -12,15 +12,17 @@ const ContactSection = () => {
         <h1 className='border-2 border-stone-300 bg-white text-2xl md:text-5xl bg-opacity-60 backdrop-blur-sm w-screen py-3 text-stone-800 font-bold text-center mb-10 drop-shadow-sm'>
           Contact</h1>
         <br />
-        <div id='contactContainer' className='w-full border-8 flex justify-evenly'>
-          <div id='socialsContainer' className='border-8 border-red-700 flex flex-col justify-end content-evenly'>
-            <div className='flex border-2 border-blue-600'>
-              <img />
-              <Link to=''>LinkedIn</Link>
-            </div>
-            <div className='flex border-2 border-blue-600'>
-              <img />
-              <Link to=''>GitHub</Link>
+        <div id='contactContainer' className='w-full flex flex-col justify-evenly'>
+          <div id='socialsContainer' className='flex flex-row justify-center content-evenly'>
+            <div className='text-lg md:text-3xl text-stone-800 font-bold text-center rounded-lg w-min flex border-2 border-stone-300 bg-stone-50 bg-opacity-70 backdrop-brightness-105 backdrop-blur-sm p-10'>
+              <Link className='mx-4 w-32 md:w-48 flex flex-col' to='https://www.linkedin.com/in/brett-kuhn/'>
+                <img className='hover:scale-105 mb-4' src='./src/assets/In-Blue-72@2x.png'/>
+                <p>LinkedIn</p>
+              </Link>
+              <Link className='mx-4 w-32 md:w-48 flex flex-col' to='https://github.com/bkuhn2'>
+                <img className='hover:scale-105 mb-4' src='./src/assets/github-mark.png'/>
+                <p>GitHub</p>
+              </Link>
             </div>
           </div>
           <form id='messageContainer' className='border-8 border-red-700'>
