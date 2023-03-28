d7911718c9c9b08029bb71cc3187eef7a2b3d22c
Brett Kuhn <brettkuhn87@gmail.com>
2023-03-27 18:44:31 -0600

    Adds styling to NavBar and uses NavLink prop to make still based on whether active

--- BAK-Portfolio/src/Components/NavBar/NavBar.tsx
+++ BAK-Portfolio/src/Components/NavBar/NavBar.tsx
@@ -6,16 +6,26 @@ const NavBar = () => {
 
 
   return (
-    <nav className='h-1/4 bg-black-texture3 bg-no-repeat bg-cover bg-center bg-fixed fixed flex-col grid justify-items-end w-full border-8 border-black'>
-      <h1 className='border-4 border-green-600 text-right text-6xl w-fit '>
+    <nav className='bg-black-texture2 bg-no-repeat bg-cover bg-center bg-local fixed flex-col grid justify-items-end w-full min-h-fit border-8 border-black shadow-2xl'>
+      <h1 className='border-4 border-green-600 text-right text-6xl w-fit text-zinc-50'>
         Hi, I'm Brett Kuhn.
       </h1>
       <section className='w-full flex-row flex justify-around'>
-        <NavLink className='text-zinc-50 border-4 border-green-600' to='/'>About</NavLink>
+        <NavLink className='text-zinc-50 text-3xl border-2 border-zinc-500 shadow-lg rounded-lg w-48 px-7 py-2 text-center h-fit backdrop-blur-sm bg-neutral-50/10 hover:scale-105' to='/'
+          style={({isActive}) => {
+            return {
+              background: isActive ? 'rgba(61, 207, 187, 0.22)' : '',
+              backdropFilter: isActive ? 'blur(6px)' : '',
+              color: isActive ? 'white' : '',
+              textShadow: isActive ? '0 0 7px #E7E7E7 , 0 0 10px #000, 0 0 15px #000, 0 0 21px #1BFFDF, 0 0 42px #29EBED, 0 0 82px #29EBED, 0 0 151px #4AC19E' : '',
+            }
+          }}
+        >ABOUT</NavLink>
         <NavLink to='/contact'>Contact</NavLink>
         <NavLink to='/projects'>Projects</NavLink>
         <NavLink to='/skills'>SKills</NavLink>
       </section>
+      <br /><br />
     </nav>
   )
 }
--- BAK-Portfolio/tailwind.config.cjs
+++ BAK-Portfolio/tailwind.config.cjs
@@ -9,7 +9,11 @@ module.exports = {
       backgroundImage: {
         'black-texture1': "url('../src/assets/annie-spratt-6a3nqQ1YwBw-unsplash.jpg')",
         'black-texture2': "url('../src/assets/andrej-lisakov-g3z-CgUiPtg-unsplash (1).jpeg')",
-        'black-texture3': "url('../src/assets/3AD0D639-0F74-4C13-8824-D5AD4B150A9A.jpeg')"
+        'black-texture3': "url('../src/assets/3AD0D639-0F74-4C13-8824-D5AD4B150A9A.jpeg')",
+        'desktop-1': "url('../src/assets/desktopA.jpg')",
+        'desktop-2': "url('../src/assets/desktopB.jpg')",
+        'desktop-3': "url('../src/assets/desktopC.jpg')",
+        'desktop-test': "linear-gradient(to right bottom, rgba('#7ed56f',0.8), rgba('#28b485',0.8)), url('../src/assets/desktopB.jpg')",
       }
     },
   },
