45654a51a2b1a16a536e833b9f5af51ae9fd39ac
Brett Kuhn <brettkuhn87@gmail.com>
2023-03-28 19:02:05 -0600

    styling to projects page and addes form with state

--- BAK-Portfolio/src/Components/ProjectsPage/ProjectsPage.tsx
+++ BAK-Portfolio/src/Components/ProjectsPage/ProjectsPage.tsx
@@ -9,7 +9,7 @@ const ProjectsPage = () => {
 
   const [allProjects, setProjects] = useState<Project[]>();
   const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState('');
+  const [searchResults, setSearchResults] = useState<Project[]>();
     
   useEffect(()=> {
     setProjects(brettProjects)
@@ -30,15 +30,24 @@ const ProjectsPage = () => {
         <h1 className='sticky top-0 z-40 border-2 border-stone-300 bg-white text-2xl md:text-5xl bg-opacity-60 backdrop-blur-sm w-screen py-3 text-stone-800 font-bold text-center mb-10 drop-shadow-sm'>
           Projects</h1>
         <br />
-        <form className='border-2 border-red-500'>
-          <p>Type in a language or technology to find any projects that match.</p>
-          <input />
+        <form className='flex flex-col items-center mb-5 text-md md:text-xl text-stone-700  text-center rounded-lg w-2/3 lg:w-1/3 border-2 border-stone-300 bg-stone-50 bg-opacity-70 backdrop-blur-sm px-10 py-6'>
+          <p className='mb-5'>Type in a language or technology to find any projects that match.</p>
+          <input 
+            className='placeholder:text-stone-500 placeholder:italic border-2 border-stone-400 rounded-md w-4/6 py-2 px-4 text-md md:text-lg bg-stone-200 bg-opacity-20 backdrop-blur-sm drop-shadow-md'
+            type='text'
+            placeholder='maybe "React," or "cypress" ...'
+            value={searchTerm}
+            onChange={event => setSearchTerm(event.target.value)}    
+          />
         </form>
         <br /><br />
+        {(searchTerm && !searchResults)&& <h2 className='text-2xl md:text-5xl text-red-700 text-center'>No results (yet!), try another or make sure word entered correctly.</h2>}
         <section className='w-full grid grid-cols-2 gap-y-10 md:gap-y-20 justify-items-center justify-evenly p-1 mb-16'>
           {/*Make conditional .. loading, no results, etc */}
-          {(!searchResults && !thumbnails) && <h2>wtf mate</h2>}
-          {(!searchResults && thumbnails) && thumbnails}
+          {(!searchResults && !thumbnails) && <h2>Loading</h2>}
+          {(!searchTerm && thumbnails) && thumbnails}
+          
+          {/*if search term AND search results... */}
         </section>
       </section>
     </>
